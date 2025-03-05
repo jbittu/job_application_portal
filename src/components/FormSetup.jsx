4f58a1edf0d17aa3981b81309a9abd7069340c25
jbittu <jaiswalbittu0319@gmail.com>
2025-03-05 11:56:59 +0530

    initate project

---
+++ src/components/FormSetup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormSetup = () => {
+  return (
+    <div>FormSetup</div>
+  )
+}
+
+export default FormSetup
---
+++ src/pages/ApplicationList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ApplicationList = () => {
+  return (
+    <div>ApplicationList</div>
+  )
+}
+
+export default ApplicationList
